9e561ef0c6faef61cdc039cfae7c594c4a92d5b6
reu <rmcheetham69@gmail.com>
2026-09-24 12:20:54 +0100

    Switch to RegEx scanner in `scanner.ts`

--- src/compiler/scanner.ts
+++ src/compiler/scanner.ts
@@ -3,41 +3,19 @@
 
 import fs from 'fs'; // File System - node module
 
+const classRegex = /\bclass\s*=\s*['"](.*?)['"]/g; // Complicated RegEx
+
+// The concept of the RegEx scanner is  to make the process simpler rather than using a massive while loop
+// Tne RegEx patterns scan the file for: class = "" class= "" class ="" and the same with single quotes
+
 const html = fs.readFileSync('examples/index.html', 'utf-8');
-
-let classIndex = html.indexOf('class');
-
+console.log(html);
+const matches = html.matchAll(classRegex);
 const classes: string[] = [];
 
-while (classIndex !== -1) {
-    let firstQuote = html.indexOf('"', classIndex);
-    let quote = '"';
-    const firstSingleQuote = html.indexOf("'", classIndex);
-
-    if (firstSingleQuote == -1 && firstQuote == -1) {
-        classIndex = html.indexOf('class', classIndex + 1);
-        continue;
-    }
-
-    if (firstSingleQuote != -1 && firstSingleQuote < firstQuote) {
-        firstQuote = firstSingleQuote;
-        quote = "'";
-    }
-
-    const secondQuote = html.indexOf(quote, firstQuote + 1);
-
-    if (secondQuote == -1) {
-        classIndex = html.indexOf('class', classIndex + 1);
-        continue;
-    }
-
-    const classContent = html.slice(firstQuote + 1, secondQuote);
-    const classNames = classContent.split(/\s+/);
-    classes.push(...classNames)
-
-    classIndex = html.indexOf('class', classIndex + 1);
+for (const match of matches) {
+    classes.push(...match[1].split(/\s+/));
 }
 
 const uniqueClasses = [...new Set(classes)];
-
 console.log(uniqueClasses);
