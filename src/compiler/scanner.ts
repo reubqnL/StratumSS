828e4341e4b95a434c049ac5e8b58f5b7b2e1352
reu <rmcheetham69@gmail.com>
2026-09-25 22:42:30 +0100

    RegEx `scanner.ts`

--- src/compiler/scanner.ts
+++ src/compiler/scanner.ts
@@ -3,19 +3,39 @@
 
 import fs from 'fs'; // File System - node module
 
-const classRegex = /\bclass\s*=\s*['"](.*?)['"]/g; // Complicated RegEx
+// The concept of the RegEx scanner is to make the process simpler rather than using a massive while loop
+// The RegEx patterns scan the file for: class = "" class= "" class ="" and the same with single quotes
 
-// The concept of the RegEx scanner is  to make the process simpler rather than using a massive while loop
-// Tne RegEx patterns scan the file for: class = "" class= "" class ="" and the same with single quotes
+function scanFile(filePath: string) {
+    const html = fs.readFileSync(filePath, 'utf-8');
 
-const html = fs.readFileSync('examples/index.html', 'utf-8');
-console.log(html);
-const matches = html.matchAll(classRegex);
-const classes: string[] = [];
+    const matches = html.matchAll(classRegex);
+    const classes: string[] = [];
 
-for (const match of matches) {
-    classes.push(...match[1].split(/\s+/));
+    for (const match of matches) {
+        classes.push(...match[1].split(/\s+/));
+    }
+
+    return classes;
 }
 
-const uniqueClasses = [...new Set(classes)];
+const classRegex = /\bclass\s*=\s*['"](.*?)['"]/g;
+
+const files = fs.readdirSync('examples');
+
+const allClasses: string[] = [];
+
+for (const file of files) {
+    if (!file.endsWith('.html')) {
+        continue;
+    }
+
+    const filePath = `examples/${file}`;
+    const fileClasses = scanFile(filePath);
+
+    allClasses.push(...fileClasses);
+}
+
+const uniqueClasses = [...new Set(allClasses)];
+
 console.log(uniqueClasses);
